6095a543904a0fcc8d96756026a51a8ad79ff409
Michelle <michelle.santiago10@gmail.com>
2020-12-24 21:22:42 -0300

    Controles pela metade

---
+++ Fireflies/Assets/3 - Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public PlayerInput controls;
+
+    private InputAction slowMotion;
+    private InputAction SlingshotMovementDirection;
+
+    private void Awake() {
+
+        //slowMotion = controls
+
+        //con
+
+        //controls.
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
