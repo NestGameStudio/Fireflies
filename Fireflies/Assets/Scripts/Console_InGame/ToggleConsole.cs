bf785dd373a342b5afd49dcda8597faa4874b2b5
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-04 15:25:21 -0300

    in game debug console + prefab

---
+++ Fireflies/Assets/Scripts/Console_InGame/ToggleConsole.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleConsole : MonoBehaviour
+{
+    [Header("Key to toggle:")]
+    public KeyCode tecla = KeyCode.L;
+    
+    private GameObject consoleObject;
+    private bool active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        consoleObject = transform.GetChild(0).gameObject;
+        consoleObject.SetActive(false);
+        active = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            active = !active;
+            consoleObject.SetActive(active);
+        }
+    }
+
+}
