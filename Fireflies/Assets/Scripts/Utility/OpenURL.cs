4b802d3762f072f1bf34c41a07b39c73c70693d0
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-04 15:33:45 -0300

    preparing for openURL button + 2d cam fix

---
+++ Fireflies/Assets/Scripts/Utility/OpenURL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public string url;
+
+    public void openURL()
+    {
+        Debug.Log("URL Opened");
+        Application.OpenURL(url);
+    }
+}
