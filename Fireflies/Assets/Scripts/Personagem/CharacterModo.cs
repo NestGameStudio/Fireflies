c2ffe64926d44faf23daf1e7cc8095882a65eabe
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-05 16:56:45 -0300

    polimentozinho no script de definicao de modo da cali 😡

--- Fireflies/Assets/Scripts/Personagem/CharacterModo.cs
+++ Fireflies/Assets/Scripts/Personagem/CharacterModo.cs
@@ -9,15 +9,18 @@ public class CharacterModo : MonoBehaviour
     public SpriteRenderer Eyes;
     public SpriteRenderer Ears;
 
+    [Header("Cores de cada modo - seguindo index do dropdown de Modo")]
     public Color[] baseColor;
     public Color[] eyesColor;
     public Color[] earsColor;
 
+    //enum que dita os modos - caso queira adicionar mais modos deve tambem aumentar os arrays de cor
     public enum modo
     {
         fogo,
         terra
     }
+    [Header("Escolha aqui o modo de Cali")]
     public modo Modo;
     
     // Start is called before the first frame update
@@ -29,6 +32,7 @@ public class CharacterModo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Aqui a cor pra cada parte da cali é colocada
         Base.color = baseColor[(int)Modo];
         Eyes.color = eyesColor[(int)Modo];
         Ears.color = earsColor[(int)Modo];
