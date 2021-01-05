e79e106c83dac6fffd033ba0af783b8423a9ce2c
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-05 16:50:58 -0300

    habilidade de mudar o modo do personagem

---
+++ Fireflies/Assets/Scripts/Personagem/CharacterModo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CharacterModo : MonoBehaviour
+{
+    public SpriteRenderer Base;
+    public SpriteRenderer Eyes;
+    public SpriteRenderer Ears;
+
+    public Color[] baseColor;
+    public Color[] eyesColor;
+    public Color[] earsColor;
+
+    public enum modo
+    {
+        fogo,
+        terra
+    }
+    public modo Modo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Modo = modo.fogo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Base.color = baseColor[(int)Modo];
+        Eyes.color = eyesColor[(int)Modo];
+        Ears.color = earsColor[(int)Modo];
+    }
+}
