dc0460095d1fd496445241e4c8e02ba02e45c381
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-06 16:47:56 -0300

    visualizacao radial - looker enemy

---
+++ Fireflies/Assets/Scripts/Inimigos/Looker_Behaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looker_Behaviour : MonoBehaviour
+{
+    /*
+    //A cada 2 segundos ele atualiza sua direção, olhando para a posição atual da Kali.
+    //Para ser derrotado, Kali deve atacá-lo por trás. Caso Kali acerte ele enquanto ele está olhando para ela, Kali perde Vida.
+    */
+
+    //tempo ate olhar para o player novamente
+    public float changeTime = 2;
+
+    //angulo de abertura de area vulneravel
+    [Range(0,360)]
+    public float angle = 60;
+
+    public SpriteRenderer parteVulneravel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (changeTime > 0)
+        {
+            changeTime -= Time.deltaTime;
+        }
+        else
+        {
+            lookAtPlayer();
+
+        }
+    }
+
+    void lookAtPlayer()
+    {
+
+    }
+}
---
+++ Fireflies/Assets/Scripts/Inimigos/Looker_Display.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Looker_Display : MonoBehaviour
+{
+    public Looker_Behaviour behaviourScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newAngle = 180 - behaviourScript.angle / 2;
+
+        behaviourScript.parteVulneravel.sharedMaterial.SetFloat("_Arc1", newAngle);
+        behaviourScript.parteVulneravel.sharedMaterial.SetFloat("_Arc2", newAngle);
+    }
+}
