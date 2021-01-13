029d3bb65963d6993d509f7d344c0a5fad8491ac
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-13 18:43:37 -0300

    Masterscene editor setup (falta ainda fazer funcionar ingame)

---
+++ Fireflies/Assets/Scripts/Level/SceneManager_Display.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class SceneManager_Display : MonoBehaviour
+{
+    SceneManager_Level sceneManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (sceneManager == null)
+        {
+            sceneManager = GetComponent<SceneManager_Level>();
+        }
+
+        if (sceneManager != null)
+        {
+
+            for (int x = 0; x < sceneManager.sceneNames.Length; x++)
+            {
+                //checa se a cena existe
+                if (EditorSceneManager.GetSceneByPath("Assets/1 - Scenes/Levels/" + sceneManager.sceneNames[x] + ".unity") != null)
+                {
+                    //carrega a cena caso ela exista
+                    EditorSceneManager.OpenScene("Assets/1 - Scenes/Levels/" + sceneManager.sceneNames[x] + ".unity", OpenSceneMode.Additive);
+                }
+
+                //Fazer a cena em referência a unica a estar ativada
+                if (x != sceneManager.startingLevel - 1)
+                {
+                    EditorSceneManager.CloseScene(EditorSceneManager.GetSceneByPath("Assets/1 - Scenes/Levels/" + sceneManager.sceneNames[x] + ".unity"), false);
+                }
+            }
+        }
+
+       
+    }
+}
---
+++ Fireflies/Assets/Scripts/Level/SceneManager_Level.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager_Level : MonoBehaviour
+{
+    //fazer mudar a cena em referencia no editor
+
+    public string[] sceneNames;
+    public int startingLevel = 1;
+
+    public KeyCode nextLevelKey;
+    public KeyCode previousLevelKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadActiveLevel();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(nextLevelKey))
+        {
+            nextLevel();
+        }
+        if (Input.GetKeyDown(previousLevelKey))
+        {
+            previousLevel();
+        }
+    }
+
+    void loadActiveLevel()
+    {
+        if (sceneNames != null)
+        {
+            //load active and unactive levels
+            for (int x = 0; x < sceneNames.Length; x++)
+            {
+                //checar se existe cena com o nome certo e se ja n foi ativada
+                if (SceneManager.GetSceneByName(sceneNames[x]) != null)
+                {
+                    SceneManager.LoadScene(sceneNames[x], LoadSceneMode.Additive);
+                }
+                if (x != startingLevel)
+                {
+                    SceneManager.UnloadScene(sceneNames[x]);
+                }
+            }
+        }
+    }
+
+    public void nextLevel()
+    {
+        startingLevel += 1;
+
+        loadActiveLevel();
+    }
+    public void previousLevel()
+    {
+        startingLevel -= 1;
+
+        loadActiveLevel();
+    }
+}
