2d51a7451bf317e63dcb504dc702362b54b8d4ae
Danny <thedannyvegas@gmail.com>
2021-01-18 20:35:41 -0300

    TimeController, StageTimer e StageTimerController funcionais, em teste

---
+++ Fireflies/Assets/3 - Scripts/UI/TimerUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerUI : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private Slider slider;
+
+    public void SetupUI(float value)
+    {
+        slider = GetComponent<Slider>();
+        if(text == null) text = GetComponentInChildren<TextMeshProUGUI>();
+        slider.maxValue = value;
+        UpdateUI(value);
+    }
+
+    public void UpdateUI(float val)
+    {
+        slider.value = val;
+        text.text = Mathf.Ceil(val).ToString();
+    }
+}
