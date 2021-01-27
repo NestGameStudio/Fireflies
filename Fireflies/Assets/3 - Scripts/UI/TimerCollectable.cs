d532d091a2621163a83449f66f7e80ace673e01c
Michelle <michelle.santiago10@gmail.com>
2021-01-27 19:44:50 -0300

    Coletável de tempo

---
+++ Fireflies/Assets/3 - Scripts/UI/TimerCollectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerCollectable : MonoBehaviour
+{
+    public float value = 5;
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.transform.CompareTag("Player")) {
+            TimerController.Instance.AddTimer(value);
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
