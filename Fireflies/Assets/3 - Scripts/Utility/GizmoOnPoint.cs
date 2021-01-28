df233ccce32cd0d5d06e1a28c51c078ff8545e09
Lucas Lima <lucaslimareis3311@gmail.com>
2021-01-28 20:07:02 -0300

    gizmo on point + minor changes

---
+++ Fireflies/Assets/3 - Scripts/Utility/GizmoOnPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoOnPoint : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(gameObject.transform.position, 0.7f);
+    }
+}
