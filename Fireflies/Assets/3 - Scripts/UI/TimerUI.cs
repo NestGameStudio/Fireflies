52336cbd88396655d860d4977b2fcda430eb6e40
Danny <thedannyvegas@gmail.com>
2021-02-01 20:04:43 -0300

    Feedbacks de tempo, fix na contagem de tempo pós respawn

--- Fireflies/Assets/3 - Scripts/UI/TimerUI.cs
+++ Fireflies/Assets/3 - Scripts/UI/TimerUI.cs
@@ -8,12 +8,15 @@ public class TimerUI : MonoBehaviour
 {
     public TextMeshProUGUI text;
     [HideInInspector] public Slider slider;
-    public Color startColor;
+    public Color defaultColor = Color.blue;
+    public Color dangerColor = Color.red;
+    private Image fillImage;
     public RectTransform feedbackRect;
     private Image feedbackImage;
 
     public void Start(){
         slider = GetComponent<Slider>();
+        fillImage = slider.fillRect.gameObject.GetComponent<Image>();
         if(text == null) text = GetComponentInChildren<TextMeshProUGUI>();
         if(feedbackRect == null) feedbackRect = transform.Find("Feedback").GetComponent<RectTransform>();
         feedbackImage = feedbackRect.gameObject.GetComponent<Image>();
@@ -37,19 +40,38 @@ public class TimerUI : MonoBehaviour
         feedbackRect.sizeDelta = new Vector2(50f,50f);
 
         feedbackImage.rectTransform.LeanCancel();
+
+        fillImage.color = defaultColor;
     }
     public void StateDanger(){
         Debug.Log("State: Danger");
 
-        feedbackRect.sizeDelta = new Vector2(150f,150f);
+        feedbackRect.sizeDelta = new Vector2(170f,170f);
         feedbackImage.color = new Color (1f,1f,1f,0f);
 
         //Começa animações loop
         feedbackRect.LeanSize(new Vector2(90f, 90f), 0.5f).setLoopClamp().setEase(LeanTweenType.easeInExpo).setIgnoreTimeScale(true);
         LeanTween.color(feedbackImage.rectTransform,new Color (1f,1f,1f,0.5f), 0.5f).setLoopClamp().setIgnoreTimeScale(true);
+
+        fillImage.color = dangerColor;
     }
 
     public void StateOver(){
         Debug.Log("State: Over");
+
+        Transform feedbackParent = feedbackRect.transform;
+
+        RectTransform feedbackBoom = Instantiate(feedbackRect.gameObject, feedbackParent.position, Quaternion.identity, feedbackParent).GetComponent<RectTransform>();
+
+        //feedbackBoom.position = new Vector3 (0f,0f,0f);
+        feedbackBoom.sizeDelta = new Vector2(90f,90f);
+        feedbackBoom.GetComponent<Image>().color = new Color (dangerColor.r,dangerColor.g,dangerColor.b,1f);
+
+        feedbackBoom.LeanSize(new Vector2(300f,300f), 0.8f).setEase(LeanTweenType.easeOutExpo).setIgnoreTimeScale(true).setOnComplete(() => destroyObj(feedbackBoom.gameObject));
+        LeanTween.color(feedbackBoom,new Color (dangerColor.r,dangerColor.g,dangerColor.b,0f), 0.8f).setIgnoreTimeScale(true);
+    }
+
+    private void destroyObj (GameObject obj){
+        Destroy(obj);
     }
 }
