1678e9923bd1abfe052f0fbfa160afe6bff4bd47
Lucas Lima <lucaslimareis3311@gmail.com>
2021-02-09 17:30:54 -0300

    roguelike camera prefab

---
+++ Fireflies/Assets/3 - Scripts/Camera/AssignCameraToPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class AssignCameraToPlayer : MonoBehaviour
+{
+    public string playerTag = "Player";
+    public CinemachineVirtualCamera CMcamera;
+
+    void Start()
+    {
+        GameObject playerGO = GameObject.FindGameObjectWithTag(playerTag).gameObject;
+
+        CMcamera.m_Follow = playerGO.transform;
+    }
+
+}
