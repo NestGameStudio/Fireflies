36644a32275cbd4055fbffcd46455fbb74a034ce
uapedro <53473286+uapedro@users.noreply.github.com>
2021-03-15 22:57:20 -0300

    Coletável cura + feedback

---
+++ Fireflies/Assets/3 - Scripts/UI/HealCollectable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealCollectable : MonoBehaviour
+{
+    private HealthManager healthManager;
+    public GameObject healParticle;
+
+    [Header("Quanto de vida recupera")]
+    public int healValue;
+
+    void Start()
+    {
+        healthManager = GameObject.FindWithTag("Player").GetComponent<HealthManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.transform.CompareTag("Player")) 
+        {
+            healthManager.maisVida(healValue);
+            if (healParticle != null)
+                healParticleTrigger();
+            Destroy(this.gameObject);
+        }
+    }
+
+    void healParticleTrigger()
+    {
+        Instantiate(healParticle,gameObject.transform.position,Quaternion.identity);
+    }
+}
