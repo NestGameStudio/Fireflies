b1bfd250a2ffd02e1b61e0c2da3757d53638482d
Danny <thedannyvegas@gmail.com>
2021-03-22 16:36:48 -0300

    EnemySpawners + Gizmos

--- Fireflies/Assets/3 - Scripts/Map/Entidades/Sub Entities/EntityEnemySpawn.cs
+++ Fireflies/Assets/3 - Scripts/Map/Entidades/Sub Entities/EntityEnemySpawn.cs
@@ -6,8 +6,16 @@ public enum EnemyType { Null, Bleeper, Looker, Random}
 
 public class EntityEnemySpawn : Entity {
 
+    [HideInInspector]
     public Vector2 SpawnPosition = Vector2.zero;
     public EnemyType EnemyType = EnemyType.Null;
+
+    [Tooltip("Chance de spawnar o indicado pelo EnemyType. Valor 1 sempre realizará spawn, 0 nunca realizará spawn")]
+    [Range(0.0f,1.0f)]
+    public float spawnChance = 1.0f;
+    public GameObject bleeperPrefab;
+    public GameObject lookerPrefab;
+    private GameObject spawnedObject; //Referência para o objeto que spawnou, se houver
 
     // Sobrescreve o método com as informações adequadas
     public override void Initialize(string name) {
@@ -35,4 +43,63 @@ public class EntityEnemySpawn : Entity {
 
         // Spawnanr moedas quando o inimigo morre
     }
+
+    public void Start(){
+        if(bleeperPrefab==null){
+            Debug.LogError("Prefab do inimigo Bleeper não tem referência");
+        }
+        if(lookerPrefab==null){
+            Debug.LogError("Prefab do inimigo Looker não tem referência");
+        }
+
+        SpawnPosition = this.transform.position;
+        SpawnRoll();
+    }
+
+    public void SpawnRoll(){
+        int enemyTypes = 2; //Bleeper e Looker
+        float roll = Random.Range(0.0f, 1.0f);
+
+        if(roll <= spawnChance){
+            //Algo será spawnado
+            switch(EnemyType){
+
+                //Spawnar Aleatório (Bleeper ou Looker)
+                case EnemyType.Random:
+                    int enemyTypeRoll = Mathf.RoundToInt(roll*100) % enemyTypes; //resulta em 0 ou 1
+                    switch(enemyTypeRoll){
+                        case 0:
+                            //Bleeper
+                            spawnedObject = SpawnEnemy(bleeperPrefab, SpawnPosition);
+                            break;
+                        case 1:
+                            //Looker
+                            spawnedObject = SpawnEnemy(lookerPrefab, SpawnPosition);
+                            break;
+                    }
+                    break;
+                
+                //Spawnar Bleeper
+                case EnemyType.Bleeper:
+                    spawnedObject = SpawnEnemy(bleeperPrefab, SpawnPosition);
+                    break;
+                
+                //Spawnar Looker
+                case EnemyType.Looker:
+                    spawnedObject = SpawnEnemy(lookerPrefab, SpawnPosition);
+                    break;
+            }
+        }
+    }
+    public GameObject SpawnEnemy(GameObject enemyPrefab, Vector2 spawnPos){
+        return Instantiate(enemyPrefab, new Vector3(SpawnPosition.x, SpawnPosition.y, 0f), Quaternion.identity, this.transform);
+    }
+
+    //Desenha um gizmo correspondente ao EnemyType
+    #if UNITY_EDITOR
+        private void OnDrawGizmos(){
+            string name = "EnemySpawner\\" + EnemyType.ToString() + ".png";
+            Gizmos.DrawIcon(transform.position, name, true);
+        }
+    #endif
 }
