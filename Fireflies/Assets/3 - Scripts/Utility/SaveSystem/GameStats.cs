0f809fc1911311a9e8a91dc0cfeb5ce999fda1a6
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2021-03-29 17:00:52 -0300

    Estatisticas do jogo e sistema de save

---
+++ Fireflies/Assets/3 - Scripts/Utility/SaveSystem/GameStats.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu]
+public class GameStats : ScriptableObject
+{
+    public int JumpCount = 0;
+}
