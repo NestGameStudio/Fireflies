46215f1ea641e1fc4034fbac82be57be7bd4759f
Danny <thedannyvegas@gmail.com>
2021-03-31 17:15:47 -0300

    Teste de Upgrade

---
+++ Fireflies/Assets/3 - Scripts/Upgrades/Upgrade.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UpgradeType {
+    JumpCharge,
+    JumpForce,
+    MaxHealth,
+    Damage
+}
+
+[CreateAssetMenu(fileName = "New Upgrade", menuName = "Upgrade")]
+public class Upgrade : ScriptableObject {
+
+    public string name;
+    public string description;
+
+    public Sprite icon;
+
+    class UpgradeStats{
+        public UpgradeType type;
+        public float amount; 
+
+        public UpgradeStats(UpgradeType upgradeType, float upgradeAmount){
+            type = upgradeType;
+            amount = upgradeAmount;
+        }
+    }
+    [SerializeField]
+    public List<UpgradeStats> stats = new List<UpgradeStats>();
+}
