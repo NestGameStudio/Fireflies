e3cdd9bbb5802669b53f7053aa1155ba34025eca
pedro <pferndd@gmail.com>
2021-04-05 20:04:58 -0300

    leantween test

--- Fireflies/Assets/3 - Scripts/UI/LeanTweenTest.cs
+++ Fireflies/Assets/3 - Scripts/UI/LeanTweenTest.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class LeanTweenTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject shop_bg, text_bg, Title, Back, Skills, Skills_bg;
+    public LeanTweenType leanTweenType;
+
     void Start()
     {
-        
+        LeanTween.scale(shop_bg.GetComponent<RectTransform>(), new Vector3(0,0,1), 0f);
+        LeanTween.scale(text_bg.GetComponent<RectTransform>(), new Vector3(0,0,1), 0f);
+        LeanTween.scale(Title.GetComponent<RectTransform>(), new Vector3(0,0,1), 0f);
+        LeanTween.scale(Back.GetComponent<RectTransform>(), new Vector3(0,0,1), 0f);
+        LeanTween.scale(Skills.GetComponent<RectTransform>(), new Vector3(0,0,1), 0f);
+        LeanTween.scale(Skills_bg.GetComponent<RectTransform>(), new Vector3(0,0,1), 0f);
+
+        LeanTween.scale(shop_bg.GetComponent<RectTransform>(), new Vector3(1,1,1), 0.5f).setEase(leanTweenType).setIgnoreTimeScale(true);
+        LeanTween.scale(text_bg.GetComponent<RectTransform>(), new Vector3(1,1,1), 0.5f).setEase(leanTweenType).setIgnoreTimeScale(true);
+        LeanTween.scale(Title.GetComponent<RectTransform>(), new Vector3(1,1,1), 0.5f).setEase(leanTweenType).setIgnoreTimeScale(true);
+        LeanTween.scale(Back.GetComponent<RectTransform>(), new Vector3(1,1,1), 0.5f).setEase(leanTweenType).setIgnoreTimeScale(true);
+        LeanTween.scale(Skills.GetComponent<RectTransform>(), new Vector3(1,1,1), 0.5f).setEase(leanTweenType).setIgnoreTimeScale(true);
+        LeanTween.scale(Skills_bg.GetComponent<RectTransform>(), new Vector3(1,1,1), 0.5f).setEase(leanTweenType).setIgnoreTimeScale(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
